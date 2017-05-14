4ee43a66a4c01fddc7097a09ee796c9d1dfeeb25
Rick <rickpowell1311@gmail.com>
2017-05-14 15:49:40 +0100

    Post frames and playback

---
+++ src/server.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Stopwatch = require("node-stopwatch").Stopwatch;
+var WebSocket = require('ws');
+
+const config = {
+    port: 8003,
+    fadecandyPort: 7890
+}
+
+var frames = [];
+var stopwatch = Stopwatch.create();
+var isConnectionOpen = false;
+var ws = {};
+
+loadFrames = function(req, res) {
+    frames = req.body.frames;
+
+    ws = new WebSocket('ws://localhost:' + config.fadecandyPort);
+    ws.on('open', function open() {
+        isConnectionOpen = true;
+    });
+    ws.on('close', function close() {
+         isConnectionOpen = false;
+    });
+    res.sendStatus(200);
+}
+
+checkConnection = function(req, res) {
+    res.send({isConnectionOpen});
+}
+
+getFrames = function(req, res) {
+    res.send({frames});
+}
+
+play = function(req, res) {
+    var frameRate = req.body.frameRate;
+
+    if (!frameRate) {
+        throw 'Framerate not provided';
+    }
+
+    var totalSeconds = frames.length / frameRate;
+    
+    console.log('Starting playback');
+    stopwatch.reset();
+    stopwatch.start();
+
+    while (stopwatch.elapsedMilliseconds < totalSeconds * 1000) {
+
+        if (!isConnectionOpen) {
+            throw 'Connection to Fadecandy is closed';
+        }
+
+        var frameNumber = Math.round(stopwatch.elapsedMilliseconds * frameRate);
+
+        var frame = frames[frameNumber];
+
+        if (frame) {
+            ws.send(frame);
+        }
+    }
+    console.log('Finished playback');
+
+    stopwatch.stop();
+    stopwatch.reset();
+    ws.close();
+    ws = {};
+    console.log('Connection closed');
+
+    res.sendStatus(201);
+}
+
+app = express();
+app.use(bodyParser());
+app.get('/api/frames', getFrames);
+app.post('/api/load', loadFrames);
+app.get('/api/checkConnection', checkConnection);
+app.post('/api/play', play);
+
+app.listen(8003, function () {
+});

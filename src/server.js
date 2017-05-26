88f5ff23a01b6a987e200948ab7a5b1ccbbdc1b3
Rick Powell <richard.powell@sfwltd.co.uk>
2017-05-26 14:22:23 +0100

    Update to include delay to sync with server time

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Stopwatch = require("node-stopwatch").Stopwatch;
 var WebSocket = require('ws');
+var moment = require('moment');
+var Client = require('node-rest-client').Client;
 
 const config = {
     port: 8003,
@@ -40,43 +42,68 @@ getFrames = function(req, res) {
 
 play = function(req, res) {
     var frameRate = req.body.frameRate;
+    var playAt = req.body.playAt;
+    var timeReferenceUrl = req.body.timeReferenceUrl;
     cancel = false;
 
     if (!frameRate) {
         throw 'Framerate not provided';
     }
 
-    var totalSeconds = frames.length / frameRate;
-    
-    console.log('Starting playback');
-    stopwatch.reset();
-    stopwatch.start();
-
-    while (stopwatch.elapsedMilliseconds < totalSeconds * 1000 && !cancel) {
-
-        if (!isConnectionOpen) {
-            throw 'Connection to Fadecandy is closed';
-        }
-
-        var frameNumber = Math.round((stopwatch.elapsedMilliseconds / 1000) * frameRate);
-
-        var frame = frames[frameNumber];
-
-        if (frame) {
-            ws.send(frame);
-            console.log('sending frame ' + frameNumber);
-        }
+    if (!playAt) {
+        throw 'Could not find a start time for playing the video';
     }
-    console.log('Finished playback');
 
-    stopwatch.stop();
-    stopwatch.reset();
-    ws.close();
-    ws = {};
-    console.log('Connection closed');
+    if (!timeReferenceUrl) {
+        throw 'Could not find URL for server time reference';
+    }
 
-    res.setHeader('Content-Type', 'application/json');
-    res.sendStatus(201);
+    var client = new Client();
+    client.get(timeReferenceUrl, function (data, response) {
+        if (!data.Successful || !data.Result) {
+            throw 'Error whilst retrieving server time';
+        }
+
+        var serverTime = moment(data.Result);
+        var startTime = moment(playAt);
+        console.log('Starting video at ' + playAt);
+
+        var delay = startTime.diff(serverTime);
+
+        setTimeout(function() {
+            var totalSeconds = frames.length / frameRate;
+        
+            console.log('Starting playback');
+            stopwatch.reset();
+            stopwatch.start();
+
+            while (stopwatch.elapsedMilliseconds < totalSeconds * 1000 && !cancel) {
+
+                if (!isConnectionOpen) {
+                    throw 'Connection to Fadecandy is closed';
+                }
+
+                var frameNumber = Math.round((stopwatch.elapsedMilliseconds / 1000) * frameRate);
+
+                var frame = frames[frameNumber];
+
+                if (frame) {
+                    ws.send(frame);
+                    console.log('sending frame ' + frameNumber);
+                }
+            }
+            console.log('Finished playback');
+
+            stopwatch.stop();
+            stopwatch.reset();
+            ws.close();
+            ws = {};
+            console.log('Connection closed');
+
+            res.setHeader('Content-Type', 'application/json');
+            res.sendStatus(201);
+        }, delay);
+    });
 }
 
 stop = function(req, res) {
